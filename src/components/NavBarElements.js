db5da3b5ac6924b76b0bcda0f1d5c2f74a0d82a8
gyulchyoung <hacha2011@naver.com>
2022-05-26 01:39:35 +0900

    add navbar

---
+++ src/components/NavBarElements.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+
+const NavBarElements = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="/">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/Profile">Profile</Nav.Link>
+      <Nav.Link href="/Alert">Alert</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  );
+}
+
+export default NavBarElements
--- src/screens/Alert.js
+++ src/screens/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert() {
   return (
-    <div><h1>Aler</h1></div>
+    <div><h1>Alert</h1></div>
   )
 }
 
