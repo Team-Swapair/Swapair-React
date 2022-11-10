77ab0a00430aa686161c5aec79e729b159ad612d
dingding-21 <phj8221@naver.com>
2022-11-11 08:04:08 +0900

    [FEAT] 회원가입, 로그인. 문자 인증 페이지 수정

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
 
 function Signup() {
-  let duplicateEmail;
-  let duplicatePhoneNumber;
+  let duplicateEmail = false;
+  let duplicatePhoneNumber = false;
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
   const navigate = useNavigate();
+  const phoneNumber = useLocation().state;
+
+  const goToPhoneSignUp = async (e) => {
+    navigate('/PhoneSignUp', { state: { email: email, password: password } });
+  };
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
@@ -20,45 +25,37 @@ function Signup() {
     setPassword(event.currentTarget.value);
   };
 
-  const onPhoneNumberHandler = (event) => {
-    setPhoneNumber(event.currentTarget.value);
-  };
-
   const onSubmitHandler = async (event) => {
     event.preventDefault();
 
-    // if (!email || !password || !phoneNumber) {
-    //   alert('빈칸이 존재합니다. 모든 항목을 채워주세요');
-    // }
-    if (!email || !password) {
+    if (!email || !password || !phoneNumber) {
       alert('빈칸이 존재합니다. 모든 항목을 채워주세요');
     } else {
-      let { data } = await axios.get(
-        `http://localhost:8080/v1/user/duplicate/email/${email}`
-      );
+      axios
+        .get(`http://localhost:8080/v1/user/duplicate/email/${email}`)
+        .catch(() => (duplicateEmail = true));
 
-      duplicateEmail = data;
-
-      // data = await axios.get(
-      //   `http://localhost:8080/v1/user/duplicate/phoneNumber/${phoneNumber}`
-      // );
-      // duplicatePhoneNumber = data.data;
+      await axios
+        .get(
+          `http://localhost:8080/v1/user/duplicate/phoneNumber/${phoneNumber}`
+        )
+        .catch(() => (duplicatePhoneNumber = true));
     }
 
     if (duplicateEmail === true)
       alert('중복된 이메일 입니다. 다시 입력해주세요');
 
-    // if (duplicatePhoneNumber === true)
-    //   alert('이미 존재하는 핸드폰 번호입니다. 다시 입력해주세요');
+    if (duplicatePhoneNumber === true)
+      alert('이미 존재하는 핸드폰 번호입니다. 다시 입력해주세요');
 
-    // if (duplicateEmail === false && duplicatePhoneNumber === false) {
-    //   await axios.post('http://localhost:8080/v1/user', {
-    //     email: email,
-    //     password: password,
-    //     phoneNumber: phoneNumber,
-    //   });
-    //   navigate('/Signin');
-    // }
+    if (duplicateEmail === false && duplicatePhoneNumber === false) {
+      await axios.post('http://localhost:8080/v1/user', {
+        email: email,
+        password: password,
+        phoneNumber: phoneNumber,
+      });
+      navigate('/Signin');
+    }
 
     if (duplicateEmail === false) {
       await axios.post('http://localhost:8080/v1/user', {
@@ -73,46 +70,55 @@ function Signup() {
   return (
     <SignupFull>
       <SignupText>
-        <h3>Signup</h3>
+        <h3>회원가입</h3>
       </SignupText>
       <form onSubmit={onSubmitHandler}>
         <SignupContainer>
-          <SignupHeader>Email</SignupHeader>
-          <SignupInput
-            name="email"
-            type="email"
-            placeholder="swapair123@gmail.com"
-            value={email}
-            onChange={onEmailHandler}
-          />
+          <SignupInput>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>이메일</Form.Label>
+              <Form.Control
+                name="email"
+                type="email"
+                placeholder="swapair123@gmail.com"
+                value={email}
+                onChange={onEmailHandler}
+              />
+            </Form.Group>
+          </SignupInput>
         </SignupContainer>
         <SignupContainer>
-          <SignupHeader>Password </SignupHeader>
-          <SignupInput
-            name="password"
-            type="password"
-            placeholder="@swapair123"
-            value={password}
-            onChange={onPasswordHandler}
-          />
+          <SignupInput>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>비밀번호</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                placeholder="@swapair123"
+                value={password}
+                onChange={onPasswordHandler}
+              />
+            </Form.Group>
+          </SignupInput>
         </SignupContainer>
         <SignupContainer>
-          <SignupHeader>PhoneNumber</SignupHeader>
-          <SignupPhoneNumberWrapper>
-            {/* <SignupInput
-              Email="핸드폰번호"
-              type="text"
-              placeholder="010-1111-2222"
-              value={phoneNumber}
-              onChange={onPhoneNumberHandler}
-            /> */}
-            <Link to="/PhoneSignUp">
-              <SignupPhoneNumberBtn type="submit">
-                인증하기
-              </SignupPhoneNumberBtn>
-            </Link>
-          </SignupPhoneNumberWrapper>
-          <button type="submit">Signup</button>
+          <SignupInput>
+            <Form.Group className="mb" controlId="formGridCity">
+              <Form.Label>전화번호</Form.Label>
+            </Form.Group>
+            <Button
+              type="submit"
+              variant="outline-dark"
+              onClick={goToPhoneSignUp}
+            >
+              인증하기
+            </Button>{' '}
+          </SignupInput>
+          <SignupBtn>
+            <div className="d-grid gap-2">
+              <Button type="submit"> 회원가입 </Button>
+            </div>
+          </SignupBtn>
         </SignupContainer>
       </form>
     </SignupFull>
@@ -143,13 +149,7 @@ const SignupContainer = styled.div`
   flex-direction: column;
 `;
 
-const SignupHeader = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-const SignupInput = styled.input`
+const SignupInput = styled.div`
   width: 300px;
   height: 50px;
   padding-left: 10px;
@@ -157,16 +157,10 @@ const SignupInput = styled.input`
   margin-top: 10px;
 `;
 
-const SignupPhoneNumberWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  position: relative;
-`;
-
-const SignupPhoneNumberBtn = styled.button`
+const SignupBtn = styled.div`
   width: 300px;
-  height: 40px;
+  height: 50px;
   padding-left: 10px;
   margin-bottom: 20px;
-  margin-top: 10px;
+  margin-top: 20px;
 `;
